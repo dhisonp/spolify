8f1784333671545374b1f62dc5f62587991fd7a4
Dhison <dhisonp@gmail.com>
2023-03-29 19:48:54 -0400

    changed hardcode params

--- server/app/recommend.py
+++ server/app/recommend.py
@@ -12,34 +12,31 @@ recommend_bp = Blueprint('recommend', __name__)
 def recommend():
     sp = check_auth()
 
+    # NOTE Temporary hardcode for playlist size
+    size = 10
     # NOTE Temporary plain defintion for parameters
     seed_tracks, seed_artists, seed_genres = [], [], []
     # NOTE Temporary hard code for sub-attributes
-    is_popular = is_acoustic = True
+    is_indie = False
+    is_acoustic = False
     # NOTE Temporary hardcode for seed_tracks
     # 1 SZA - Good Days
-    # 2 Mac Ayres - Easy
-    # 3 J. Passion & J. Barrera - Constant
-    # 4 Ella Mai - A Mess
-    # 5 DaniLeigh & Chris Brown - Easy
+    # 2 ASTN - Happier Than Ever
+    # 3 SZA - Snooze
     seed_tracks.append(
         'https://open.spotify.com/track/3YJJjQPAbDT7mGpX3WtQ9A?si=3869a3f1df9346ae')
     seed_tracks.append(
-        'https://open.spotify.com/track/58dSdjfEYNSxte1aNVxuNf?si=03d77f85c4174206')
+        'https://open.spotify.com/track/7es56D0QedGMsK39wl2yFX?si=e965e0f056f44954')
     seed_tracks.append(
-        'https://open.spotify.com/track/12ZYXSzFJUSrFyO1u3Ylqx?si=1741e807b99a46bc')
-    seed_tracks.append(
-        'https://open.spotify.com/track/3HgJ1aYlEgy8Yj1NMAe1pm?si=4113d925da8b4c99')
-    seed_tracks.append(
-        'https://open.spotify.com/track/4CMrdHWqic0usIZfTrKoI3?si=2a14fa6152344d8c')
+        'https://open.spotify.com/track/4iZ4pt7kvcaH6Yo8UoZ4s2?si=da2e57f816af4e2c')
     # NOTE Temporary hardcode for genres
     seed_genres.append('r-n-b')
     # Recommendation API
     # Define sub attributes
     min_acousticness = 0.6 if is_acoustic else 0.0
-    max_popularity = 40 if (is_popular is None) else 100
+    max_popularity = 40 if is_indie else 100
     results = sp.recommendations(
-        limit=10,
+        limit=size,
         seed_tracks=seed_tracks,
         min_acousticness=min_acousticness,
         max_popularity=max_popularity
