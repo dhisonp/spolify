14d89e6e6a7ed64c4dca37faf57bc4533adad410
Dhison <dhisonp@gmail.com>
2023-04-19 23:49:06 -0400

    fixed mobile generate

--- client/pages/generatePlaylist.tsx
+++ client/pages/generatePlaylist.tsx
@@ -143,140 +143,138 @@ const GeneratePlaylist = () => {
         <title>Generate Playlist</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
+      <Header />
 
-      <main className="flex flex-col justify-center h-full pt-16 overflow-hidden">
-        <div className="w-full">
-          <form
-            className="flex flex-col justify-center items-center"
-            onSubmit={handleSubmit}
-          >
-            <div className="flex flex-col justify-center">
+      <main className="flex flex-col justify-between overflow-auto h-full pt-16 pb-8">
+        <div className="text-gray-500 mb-3 text-center">
+          <button onClick={handleGuide}>
+            <p className="text-gray-500 text-base hover:text-gray-400 mb-2 mt-4">
+              {operationGuideString.p1}&rarr;
+            </p>
+          </button>
+          {displayHelper && (
+            <div className="">
+              <p>{operationGuideString.p2}</p>
+              <p>{operationGuideString.p3}</p>
+              <p>{operationGuideString.p4}</p>
+              <p>{operationGuideString.p5}</p>
+              <span className="text-sm text-gray-400">
+                {operationGuideString.p6}
+              </span>
+            </div>
+          )}
+        </div>
+        <form
+          className="flex flex-col justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex flex-col justify-center">
+            <InputField
+              placeholder="Name of the playlist."
+              label="Title"
+              className="mt-2"
+              name="title"
+              value={formValues.title}
+              onChange={handleChange}
+            />
+            <div className="items-center flex mt-2">
+              <label className="text-lg font-medium mr-12">Size</label>
+              <span className="text-gray-500 mr-4 text-lg w-4">
+                {formValues.size}
+              </span>
+              <input
+                type="range"
+                min="5"
+                max="30"
+                name="size"
+                value={formValues.size}
+                onChange={handleChange}
+                className="cursor-pointer range accent-fuchsia-700 w-3/5"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col sm:flex-row justify-center items-center mt-4 sm:mt-8">
+            <div className="flex flex-col p-4">
               <InputField
-                placeholder="Name of the playlist."
-                label="Title"
-                className="mt-2"
-                name="title"
-                value={formValues.title}
+                placeholder={fieldPlaceholder}
+                label="1. "
+                name="uri1"
+                value={formValues.uris.uri1}
                 onChange={handleChange}
               />
-              <div className="items-center flex mt-2">
-                <label className="text-lg font-medium mr-12">Size</label>
-                <span className="text-gray-500 mr-4 text-lg w-4">
-                  {formValues.size}
-                </span>
+              <InputField
+                placeholder={fieldPlaceholder}
+                label="2. "
+                name="uri2"
+                value={formValues.uris.uri2}
+                onChange={handleChange}
+              />
+              <InputField
+                placeholder={fieldPlaceholder}
+                label="3. "
+                name="uri3"
+                value={formValues.uris.uri3}
+                onChange={handleChange}
+              />
+              <InputField
+                placeholder={fieldPlaceholder}
+                label="4. "
+                name="uri4"
+                value={formValues.uris.uri4}
+                onChange={handleChange}
+              />
+              <InputField
+                placeholder={fieldPlaceholder}
+                label="5. "
+                name="uri5"
+                value={formValues.uris.uri5}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex flex-col mx-4">
+              <li className="flex flex-row mb-2 py-2 items-center justify-center">
                 <input
-                  type="range"
-                  min="5"
-                  max="30"
-                  name="size"
-                  value={formValues.size}
+                  className="mr-2 align-middle"
+                  type="checkbox"
+                  name="isAcoustic"
+                  checked={formValues.isAcoustic}
                   onChange={handleChange}
-                  className="cursor-pointer range accent-fuchsia-700 w-3/5"
                 />
-              </div>
+                <label className="text-lg font-medium">Acoustic</label>
+              </li>
+              <li className="flex flex-row mb-2 py-2 items-center justify-center">
+                <input
+                  className="mr-2 align-middle"
+                  type="checkbox"
+                  name="isIndie"
+                  checked={formValues.isIndie}
+                  onChange={handleChange}
+                />
+                <label className="text-lg font-medium">Unpopular</label>
+              </li>
             </div>
-            <div className="flex flex-col sm:flex-row justify-center items-center mt-8">
-              <div className="flex flex-col p-4">
-                <InputField
-                  placeholder={fieldPlaceholder}
-                  label="1. "
-                  name="uri1"
-                  value={formValues.uris.uri1}
-                  onChange={handleChange}
-                />
-                <InputField
-                  placeholder={fieldPlaceholder}
-                  label="2. "
-                  name="uri2"
-                  value={formValues.uris.uri2}
-                  onChange={handleChange}
-                />
-                <InputField
-                  placeholder={fieldPlaceholder}
-                  label="3. "
-                  name="uri3"
-                  value={formValues.uris.uri3}
-                  onChange={handleChange}
-                />
-                <InputField
-                  placeholder={fieldPlaceholder}
-                  label="4. "
-                  name="uri4"
-                  value={formValues.uris.uri4}
-                  onChange={handleChange}
-                />
-                <InputField
-                  placeholder={fieldPlaceholder}
-                  label="5. "
-                  name="uri5"
-                  value={formValues.uris.uri5}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="flex flex-col mx-4">
-                <div className="flex flex-row mb-2">
-                  <input
-                    className="mr-4"
-                    type="checkbox"
-                    name="isAcoustic"
-                    checked={formValues.isAcoustic}
-                    onChange={handleChange}
-                  />
-                  <label className="text-lg font-medium">Acoustic</label>
-                </div>
-                <div className="flex flex-row mb-2">
-                  <input
-                    className="mr-4"
-                    type="checkbox"
-                    name="isIndie"
-                    checked={formValues.isIndie}
-                    onChange={handleChange}
-                  />
-                  <label className="text-lg font-medium">Unpopular</label>
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-col sm:flex-row justify-between items-center">
-              <button
-                className="m-2 border-2 py-2 px-4 border-emerald-600 rounded-full hover:bg-emerald-600 hover:text-gray-100 transition duration-200"
-                onClick={handleBack}
-              >
-                <span className="font-bold">&larr; Back</span>
-              </button>
-              <button
-                className="m-2 border-2 py-2 px-4 border-emerald-600 rounded-full hover:bg-emerald-600 hover:text-gray-100 transition duration-200"
-                type="reset"
-              >
-                <span className="font-bold">Reset</span>
-              </button>
-              <button
-                className="m-2 border-2 py-2 px-4 border-emerald-700 rounded-full bg-emerald-700 text-gray-100 transition duration-200 hover:border-emerald-300 hover:bg-emerald-300 hover:text-fuchsia-900"
-                type="submit"
-              >
-                <span className="font-bold">Generate &rarr;</span>
-              </button>
-            </div>
-          </form>
-          <div className="text-gray-500 mb-3 text-center italic">
-            <button onClick={handleGuide}>
-              <p className="text-gray-500 italic text-base hover:text-gray-400 mb-2 mt-4">
-                {operationGuideString.p1}&rarr;
-              </p>
-            </button>
-            {displayHelper && (
-              <div className="">
-                <p>{operationGuideString.p2}</p>
-                <p>{operationGuideString.p3}</p>
-                <p>{operationGuideString.p4}</p>
-                <p>{operationGuideString.p5}</p>
-                <span className="text-sm text-gray-400">
-                  {operationGuideString.p6}
-                </span>
-              </div>
-            )}
           </div>
-        </div>
+          <div className="flex flex-col sm:flex-row justify-between items-center">
+            <button
+              className="m-2 border-2 py-2 px-4 border-emerald-600 rounded-full hover:bg-emerald-600 hover:text-gray-100 transition duration-200"
+              onClick={handleBack}
+            >
+              <span className="font-bold">&larr; Back</span>
+            </button>
+            <button
+              className="m-2 border-2 py-2 px-4 border-emerald-600 rounded-full hover:bg-emerald-600 hover:text-gray-100 transition duration-200"
+              type="reset"
+            >
+              <span className="font-bold">Reset</span>
+            </button>
+            <button
+              className="m-2 border-2 py-2 px-4 border-emerald-700 rounded-full bg-emerald-700 text-gray-100 transition duration-200 hover:border-emerald-300 hover:bg-emerald-300 hover:text-fuchsia-900"
+              type="submit"
+            >
+              <span className="font-bold">Generate &rarr;</span>
+            </button>
+          </div>
+        </form>
       </main>
     </div>
   );
